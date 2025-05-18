2064b2f2d420d1e59f9e534e7fbb55a45825f30f
zyricCO <zyricco@gmail.com>
2025-05-18 06:09:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,4 +69,6 @@ def home():
     return "Bot is running!"
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
